03d760f8a1381614b254d88a2a24c9c2631b3d92
xanian7 <xanian407@gmail.com>
2024-09-03 18:05:53 -0400

    fixing base directory

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/xanderkraemer.com/',
   plugins: [
     vue(),
     vueDevTools(),
